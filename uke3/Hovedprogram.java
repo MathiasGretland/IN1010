01302652745eb731e84c59850a22d94a31e54108
MathiasGretland <mathiasgretland@gmail.com>
2021-01-25 12:50:11 +0100

    Starten på uke 3

--- uke3/Hovedprogram.java
+++ uke3/Hovedprogram.java
@@ -5,6 +5,9 @@ public class Hovedprogram {
         String tittel = "Dont go breaking my heart";
         String[] artister = new String[]{"Elton John", "Kiki Dee"};
         Sang sang1 = new Sang(tittel, artister);
+        Sang sang2 = new Sang(tittel, artister);
         Spilleliste sp = new Spilleliste("liste");
+        sp.leggTilSang(sang1);
+        sp.leggTilSang(sang2);
     }
 }
