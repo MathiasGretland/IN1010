3abf211c132803590e59253c8cea9725231bbd55
MathiasGretland <mathiasgretland@gmail.com>
2021-01-25 14:00:22 +0100

    Spilleliste lager med noen småfeil

--- uke3/Sang.java
+++ uke3/Sang.java
@@ -1,5 +1,38 @@
 package uke3;
 
 public class Sang {
+    private String[] artister;
+    private String tittel;
+
+    public Sang(String tittel, String [] artister){
+        this.artister = artister;
+        this.tittel = tittel;
+    }
+
+    public void spill() {
+        System.out.println("Spiller " + tittel);
+    }
+
+    public boolean sjekkArtister(String navn) {
+        for (String artist : artister){
+            if (artist.toLowerCase().equals(navn.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String hentTittel(){
+        return tittel;
+    }
+
+    public String[] hentArtister(){
+        return artister;
+    }
+
+    public String toString(){
+        return tittel + " av " + artister[0];
+    }
+
 
 }
--- uke3/Spilleliste.java
+++ uke3/Spilleliste.java
@@ -1,4 +1,57 @@
 package uke3;
 
+import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class Spilleliste {
+    private String navn;
+    private ArrayList<Sang> sanger;
+
+    public Spilleliste(String listenavn){
+        navn = listenavn;
+        sanger = new ArrayList<Sang>();
+    }
+
+    public void lesFraFil(String filnavn){
+        File fil = new File(filnavn);
+        Scanner in;
+        try{
+            in = new Scanner(fil);
+        }catch (FileNotFoundException exception){
+            System.out.println("Fant ikke filen");
+            in = new Scanner("");
+        }
+        while (in.hasNextLine()){
+            String linje = in.nextLine();
+            String[] alleData = linje.split(";");
+            //tittel;Elton John, Kiki Dee
+            String[] artistene = alleData[1].split(",");
+            Sang nySang = new Sang(alleData[0], artistene);
+            sanger.add(nySang);
+        }
+    }
+
+    public void leggTilSang(Sang sang){
+        sanger.add(sang);
+    }
+
+    public void fjernSang(Sang sang){
+        sanger.remove(sang);
+    }
+
+    public void spillSang(Sang sang){
+        sang.spill();
+    }
+
+    public void spillAlle(){
+        for (int i = 0; i < sanger.size(); i++){
+            sanger.get(i).spill();
+        }
+    }
+
+
+
+
 }
