e0595cbc596679b82185c709e13b23b0a615f37f
MathiasGretland <mathiasgretland@gmail.com>
2021-03-17 15:28:18 +0100

    masse

---
+++ uke9/MinRun.java
@@ -0,0 +1,36 @@
+package uke9;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MinRun implements Runnable{
+    private final int SLUTT;
+    private final int START;
+    private final int N;
+    private int teller = 0;
+    private final NteTall.NteMonitor monitor;
+
+    private final Lock lock = new ReentrantLock();
+
+    public MinRun(int n, int start, int slutt, NteTall.NteMonitor m){
+        this.SLUTT = slutt;
+        this.START = start;
+        this.N = n;
+        this.monitor = m;
+    }
+
+    @Override
+    public void run() {
+        lock.lock();
+        try {
+            while (teller < SLUTT){
+                if (teller % 10 == N){
+                    monitor.skrivUt(teller);
+                }
+                teller++;
+            }
+        }finally {
+            lock.unlock();
+        }
+    }
+}
