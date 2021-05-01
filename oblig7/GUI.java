72f57f9163567f5938fa40e68d7aede978f1abfa
MathiasGretland <mathiasgretland@gmail.com>
2021-05-01 23:48:49 +0200

    Ferdig med oblig7

--- oblig7/GUI.java
+++ oblig7/GUI.java
@@ -6,8 +6,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Oppretter klassen GUI
+ */
 public class GUI {
     public static void main(String[] args) throws FileNotFoundException {
         JFrame vindu = new JFrame("Labyrint");
@@ -19,35 +23,68 @@ public class GUI {
         }
         File file = velger.getSelectedFile();
         Labyrint lab = new Labyrint(file);
-
-
         Labyrinten brett = new Labyrinten(file, lab);
         brett.initGUI();
         vindu.add(brett);
-
         vindu.pack();
         vindu.setVisible(true);
     }
 }
 
-
-
+/**
+ * Oppretter klassen Labyrinten som er en subklasse av klassen JPanel
+ */
 class Labyrinten extends JPanel{
     Brettet brettet;
     Labyrint lab;
     File filnavn;
+    JButton avslutt;
+    JLabel info;
+    JButton reset;
 
-
+    /**
+     * Konstruktøren obretter et brett, inne i konstruktøren.
+     * @param filnavn Tar inn en File
+     * @param lab Tar inn en Labyrint
+     * @throws FileNotFoundException Sender den videre
+     */
     Labyrinten (File filnavn, Labyrint lab) throws FileNotFoundException {
         this.filnavn = filnavn;
         this.lab = lab;
         brettet = new Brettet(this);
     }
 
+    /**
+     * Metoden initGUI oppretter får innholdet til brettet på vinduet (vindu som opprettes i hovedprogrammet) samt. noen buttons og tekst.
+     */
     void initGUI (){
         //Her skal det skje mye
         brettet.initGUI();
         add(brettet);
+
+        info = new JLabel("Velg en hvit rute!");
+        add(info);
+
+        reset = new JButton("NULLSTILL");
+        class Reset implements ActionListener{
+            @Override
+            public void actionPerformed (ActionEvent e){
+                brettet.reset();
+            }
+        }
+        reset.addActionListener(new Reset());
+        add(reset);
+
+        avslutt = new JButton("EXIT");
+        class Stopp implements ActionListener{
+            @Override
+            public void actionPerformed (ActionEvent e){
+                System.exit(0);
+            }
+        }
+        avslutt.addActionListener(new Stopp());
+        add(avslutt);
+
     }
 
     File hentFil(){
@@ -59,7 +96,9 @@ class Labyrinten extends JPanel{
     }
 }
 
-
+/**
+ * Oppretter klassen Brettet som er en subklasse av klassen JPanel
+ */
 class Brettet extends JPanel{
     Labyrinten labyrinten;
     Labyrint lab;
@@ -69,6 +108,11 @@ class Brettet extends JPanel{
     int rad;
     int kolonne;
 
+    /**
+     * Konstruktøren til Brettet lager brettet basert på hva den leser inn fra filen som brukeren velger.
+     * @param labyrinten Tar inn en Labyrinten
+     * @throws FileNotFoundException Sender den videre
+     */
     Brettet (Labyrinten labyrinten) throws FileNotFoundException {
         this.labyrinten = labyrinten;
         this.fil = labyrinten.hentFil();
@@ -77,12 +121,15 @@ class Brettet extends JPanel{
         lagBrett();
     }
 
+    /**
+     * Metoden lagBrett er en kopi av lesInn fra Labyrint klassen, men den er gjort om slik at den passer programmet.
+     */
     void lagBrett(){
         String radOgKolonne = input.nextLine();
         String[] data = radOgKolonne.split(" ");
         rad = Integer.parseInt(data[0]);
         kolonne = Integer.parseInt(data[1]);
-        knapper = new Knapp[rad][kolonne]; //Opprettelsen av ruteArray (Dette kunne også gjort øverst i programmet).
+        knapper = new Knapp[rad][kolonne];
         int radInne = 0;
         int kolonneInne = 0;
         while (input.hasNext()) {
@@ -90,14 +137,14 @@ class Brettet extends JPanel{
             for (int i = 0; i < ruter.length(); i++) {
                 char tegn = ruter.charAt(i);
                 if (tegn == '#') {
-                    SortKnapp sortRute = new SortKnapp(radInne, kolonneInne);
+                    SortKnapp sortRute = new SortKnapp(radInne, kolonneInne, this);
                     knapper[radInne][kolonneInne] = sortRute;
                 } else if (tegn == '.') {
                     if (radInne == 0 || radInne == (ruter.length() - 1) || kolonneInne == 0 || kolonneInne == (ruter.length() - 1)) {
-                        AapningKnapp aapning = new AapningKnapp(radInne, kolonneInne);
+                        AapningKnapp aapning = new AapningKnapp(radInne, kolonneInne, this);
                         knapper[radInne][kolonneInne] = aapning;
                     } else {
-                        HvitKnapp hvitRute = new HvitKnapp(radInne, kolonneInne);
+                        HvitKnapp hvitRute = new HvitKnapp(radInne, kolonneInne, this);
                         knapper[radInne][kolonneInne] = hvitRute;
                     }
                 }
@@ -112,6 +159,9 @@ class Brettet extends JPanel{
         input.close();
     }
 
+    /**
+     * Metoden initGUI setter dimensjonene samt legger til knappene
+     */
     void initGUI (){
         setLayout(new GridLayout(rad, kolonne));
 
@@ -123,111 +173,130 @@ class Brettet extends JPanel{
         }
     }
 
-    /*
-    boolean reset(){
-        boolean funnet = true;
-
-    }
-
+    /**
+     * Metdoen reset() setter brettet tilbake til slik det så ut, etter at det først ble opprettet.
      */
-
-    Knapp hentKnapp(int knappRad, int knappKolonne){
-        return knapper[rad][kolonne];
-    }
-
-    Knapp[][] hentKnapper(){
-        return knapper;
+    void reset(){
+        for (Knapp[] k: knapper){
+            for (Knapp r : k){
+                if (r instanceof HvitKnapp){
+                    r.setBackground(Color.WHITE);
+                }
+                else if (r instanceof AapningKnapp){
+                    r.setBackground(Color.WHITE);
+                }
+            }
+        }
+        labyrinten.info.setText("Velg en hvit rute!");
     }
 }
 
+/**
+ * Oppretter klassen Knapp som er en subklasse av klassen JButton
+ */
 class Knapp extends JButton{
     Brettet brett;
     int rad;
     int kolonne;
 
 
-    Knapp (int rad, int kolonne){
+    Knapp (int rad, int kolonne, Brettet brett){
         this.rad = rad;
         this.kolonne = kolonne;
+        this.brett = brett;
     }
 
+    /**
+     * Metoden initGUI er der slik at knappene får, riktig størrelse, farge og utfører ActionListener
+     */
     void initGUI (){
     }
 }
 
+/**
+ * Oppretter klassen SortKnapp som er en subklasse av klassen Knapp
+ */
 class SortKnapp extends Knapp{
 
-    SortKnapp(int rad, int kolonne) {
-        super(rad, kolonne);
+    SortKnapp(int rad, int kolonne, Brettet brett) {
+        super(rad, kolonne, brett);
     }
 
+    /**
+     * Metoden initGUI er der slik at knappene får, riktig størrelse, farge og utfører ActionListener
+     */
     void initGUI (){
         setBorder(BorderFactory.createLineBorder(Color.black));
-        setFont(new Font("Monospaced", Font.BOLD, 15));
         setPreferredSize(new Dimension(60, 60));
         setBackground(Color.BLACK);
-
-
-        Knapp denneKnappen = this;
-        class SortKnappvelger implements ActionListener{
-            @Override
-            public void actionPerformed (ActionEvent e) {
-                String radTekst = String.valueOf(rad);
-                String kolonneTekst = String.valueOf(kolonne);
-                setText("(" + radTekst + "," + kolonneTekst+ ")");
-            }
-        }
-        addActionListener(new SortKnappvelger());
     }
 }
 
+/**
+ * Oppretter klassen HvitKnapp som er en subklasse av klassen Knapp
+ */
 class HvitKnapp extends Knapp{
-
-    HvitKnapp(int rad, int kolonne) {
-        super(rad, kolonne);
+    int antallUtveier;
+    HvitKnapp(int rad, int kolonne, Brettet brett) {
+        super(rad, kolonne, brett);
     }
 
+    /**
+     * Metoden initGUI er der slik at knappene får, riktig størrelse, farge og utfører ActionListener
+     */
     void initGUI (){
         setBorder(BorderFactory.createLineBorder(Color.black));
-        setFont(new Font("Monospaced", Font.BOLD, 15));
         setPreferredSize(new Dimension(60, 60));
         setBackground(Color.WHITE);
 
-
-        Knapp denneKnappen = this;
         class HvitKnappvelger implements ActionListener{
             @Override
             public void actionPerformed (ActionEvent e) {
-                String radTekst = String.valueOf(rad);
-                String kolonneTekst = String.valueOf(kolonne);
-                setText("(" + radTekst + "," + kolonneTekst+ ")");
-                brett.lab.finnUtveiFra(denneKnappen.rad, denneKnappen.kolonne);
+                /**
+                 * Finner alle utveiene og skriver ut det, samt. Gjør stien fra den HviteRuten til utveien tydelig med å gjøre rutene grønne
+                 */
+                ArrayList<ArrayList<Tuppel>> utveier = brett.lab.finnUtveiFra(rad,kolonne);
+                antallUtveier = utveier.size();
+                ArrayList<Tuppel> kjappeste = utveier.get(0);
+                for (Tuppel p : kjappeste){
+                    int tuppelRad = p.rad;
+                    int tuppelKolonne = p.kolonne;
+                    for (Knapp[] k : brett.knapper){
+                        for (Knapp r : k){
+                            if (r.rad == tuppelRad && r.kolonne == tuppelKolonne){
+                                r.setBackground(Color.GREEN);
+                            }
+                        }
+                    }
+                }
+                brett.labyrinten.info.setText("Labyrinten fant: " + antallUtveier + " utveier. " + "Viser den korteste utveien.");
             }
         }
         addActionListener(new HvitKnappvelger());
     }
 }
 
+/**
+ * Oppretter klassen AapningKnapp som er en subklasse av klassen Knapp
+ */
 class AapningKnapp extends Knapp{
 
-    AapningKnapp(int rad, int kolonne) {
-        super(rad, kolonne);
+    AapningKnapp(int rad, int kolonne, Brettet brett) {
+        super(rad, kolonne, brett);
     }
 
+    /**
+     * Metoden initGUI er der slik at knappene får, riktig størrelse, farge og utfører ActionListener
+     */
     void initGUI (){
         setBorder(BorderFactory.createLineBorder(Color.black));
-        setFont(new Font("Monospaced", Font.BOLD, 15));
         setPreferredSize(new Dimension(60, 60));
         setBackground(Color.WHITE);
 
-
-        Knapp denneKnappen = this;
         class AapningKnappvelger implements ActionListener{
             @Override
             public void actionPerformed (ActionEvent e) {
-                String radTekst = String.valueOf(rad);
-                String kolonneTekst = String.valueOf(kolonne);
-                setText("(" + radTekst + "," + kolonneTekst+ ")");
+                brett.labyrinten.info.setText("Dette er en åpning! Trykk på en annen hvit rute!");
             }
         }
         addActionListener(new AapningKnappvelger());
