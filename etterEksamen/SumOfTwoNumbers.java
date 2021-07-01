d58a338efafc729f2093915886bc5d3e435b48b3
MathiasGretland <mathiasgretland@gmail.com>
2021-07-01 19:46:19 +0200

    Etter eksamen

--- etterEksamen/SumOfTwoNumbers.java
+++ etterEksamen/SumOfTwoNumbers.java
@@ -18,4 +18,104 @@ public class SumOfTwoNumbers {
             return 0;
         }
     }
+
+    public static int countTrue(boolean[] array){
+        int teller = 0;
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == true){
+                teller++;
+            }
+        }
+        return teller;
+    }
+
+    public static int triArea(int a, int b){
+        return ((a * b) /2);
+    }
+
+    public static int remainder(int a, int b){
+        return (a%b);
+    }
+
+    public static int addition(int a){
+        return (a+1);
+    }
+
+    public static String nameShuffle(String a){
+        String[] data = a.split(" ");
+        String nyttNavn = "";
+        for (int i = data.length-1; i >= 0; i--){
+            nyttNavn += data[i] + " ";
+        }
+        return nyttNavn;
+    }
+
+    public static int differenceMaxMin(int[] array){
+        int storst = array[0];
+        int minst = array[0];
+        for (int i = 0; i < array.length; i++){
+            if (array[i] < minst){
+                minst = array[i];
+            }
+            if (array[i] > storst){
+                storst = array[i];
+            }
+        }
+        return storst - minst;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(nameShuffle("1 2 3 4"));
+        int[] array = new int[]{10,4,1,2,8,91};
+        System.out.println(differenceMaxMin(array));
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
